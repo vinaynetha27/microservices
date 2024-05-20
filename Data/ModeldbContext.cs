18b41130b9d3ed159069d03c3a694d582e99931f
vinay.m <vinay.m@SBSLPT-28>
2024-05-20 12:32:16 +0530

    Add project files.

---
+++ Data/ModeldbContext.cs
@@ -0,0 +1,13 @@
+﻿using microservices.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace microservices.Data
+{
+    public class ModeldbContext : DbContext
+    {
+        public ModeldbContext(DbContextOptions<ModeldbContext> options): base(options) {
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ Data/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace microservices.Data.Models
+{
+    public class Products
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
